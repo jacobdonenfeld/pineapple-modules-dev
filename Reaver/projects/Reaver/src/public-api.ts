1535f9e68637adf629d651e8b7d75cdc39420fd9
Jacob Donenfeld <jd@projectn.co>
2022-04-12 16:49:29 -0700

    Start tuning module to work with reaver. Mostly renaming

--- Reaver/projects/Reaver/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of examplemodule
- */
-
-export * from './lib/services/example-service.service';
-export * from './lib/components/example-module.component';
-export * from './lib/example-module.module';
---
+++ reaver/projects/reaver/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of reaver
+ */
+
+export * from './lib/services/reaver.service';
+export * from './lib/components/reaver.component';
+export * from './lib/reaver.module';
